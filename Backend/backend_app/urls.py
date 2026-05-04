87dd7546d55e86c73b6f2cec685158df7751c823
Abhay K S <abhayks561@gmail.com>
2026-05-04 11:35:48 +0530

    merged doctors and gallery section

--- Backend/backend_app/urls.py
+++ Backend/backend_app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import GalleryListCreateAPIView, GalleryDetailAPIView
+from .views import specialist_list, specialist_detail
 
 urlpatterns = [
     path('gallery/', GalleryListCreateAPIView.as_view(), name='gallery-list-create'),
     path('gallery/<int:pk>/', GalleryDetailAPIView.as_view(), name='gallery-detail'),
+
+    path('specialists/', specialist_list),
+    path('specialists/<int:pk>/', specialist_detail)
+
 ]
